ff1ad21ffc4aa72936c4b29d6fb7368cce7ac462
shadowmoonezios <agushifelde@hotmail.com>
2025-08-15 13:54:34 +0300

    Add comment for MongoDB connection

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// MongoDB Verbindung
+// MongoDB Verbindung und Fehlerbehandlung
 mongoose.connect('mongodb://localhost:27017/personal-book-library', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB verbunden'))
   .catch(err => console.log(err));
